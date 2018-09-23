1707da79e2fcd9d9e669afd4c8b4bf190491fad3
CLenin <36728380+CLenin@users.noreply.github.com>
2018-09-23 11:01:37 +0300

    Files

---
+++ MyLibrary/Methods.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyLibrary
+{
+    public class Methods
+    {
+        public static string Return(string path, char toRemoveAfter)
+        {
+            var pathArr = path.ToCharArray();
+            var index = new int();
+            for (var i = pathArr.Length - 1; i > 0; i--)
+            {
+                if (pathArr[i] == toRemoveAfter)
+                {
+                    index = i;
+                    i = 0;
+                }
+            }
+            path = path.Remove(index);
+            if (path.Length <= 2)
+                path = path + toRemoveAfter;
+            return path;
+        }
+    }
+}
